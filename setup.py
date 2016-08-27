2fab294448dd9c07e8b1b9eea9edd1653d03d33f
wwidurek <wwidurek@gmail.com>
2016-08-28 00:07:21 +0100

    final packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # python setup.py --dry-run --verbose install
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
@@ -8,12 +8,9 @@ setup(
     version='0.5.2', # Should be updated with new versions
     author='Wojtek Widurek',
     author_email='wwidurek@gmail.com',
-    py_modules=['wirelesstag'],
     packages=['wirelesstag'],
-    scripts=[],
-    data_files=[],
     url='https://github.com/wwidurek/wirelesstag',
     license='Open Source',
     description='Simple API to access WirelessTag data from any python script.',
-    long_description=open('README.md').read()
+    zip_safe=False
 )
